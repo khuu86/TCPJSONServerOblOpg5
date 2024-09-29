4a9dbcb23fb61c8922ae345f228b1b0d1d468cb7
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-09-29 21:06:01 +0200

    Fjernet kommenteret kode og fikset små fejl

--- TCPJSONServerOblOpg5/Program.cs
+++ TCPJSONServerOblOpg5/Program.cs
@@ -26,14 +26,11 @@ void HandleClient(TcpClient socket)
     StreamReader reader = new StreamReader(ns);
     StreamWriter writer = new StreamWriter(ns);
 
-    int counter = 0;
-
     while (socket.Connected)
     {
-        string message = reader.ReadLine().ToLower(); // Modtager besked fra client (i lowercase)
+        string message = reader.ReadLine(); // Modtager besked fra client (i lowercase)
         Console.WriteLine("JESON Besked modtaget: " + message);
 
-        counter++;
         try
         {
             Request? request = JsonSerializer.Deserialize<Request>(message);
@@ -72,98 +69,57 @@ void HandleClient(TcpClient socket)
             SendError("Invalid JSON", writer);
         }
     }
+    socket.Close();
 }
 // Method to handle the "stop" command
 void HandleStop(StreamWriter writer, TcpClient socket)
 {
-    writer.WriteLine("Goodbye: Closing down Client");
+    // Send a message to the client confirming the disconnection
+    writer.WriteLine(JsonSerializer.Serialize(new { Message = "Goodbye: Closing down Client" }));
     writer.Flush();
+
+    // Log that the client has disconnected
     Console.WriteLine("Client connection closed.");
+
+    // Close the client socket, but do not stop the server
     socket.Close();
 }
 
 // Method to handle the "random" command
 void HandleRandom(Request request, StreamWriter writer)
 {
-
-
-    //writer.WriteLine("Enter two random numbers: x y");
-    //writer.Flush();
-
-    //string[] messageA = reader.ReadLine().Split(" ");
-    //int x = Int32.Parse(messageA[0]);
-    //int y = Int32.Parse(messageA[1]);
-
     int minValue = Math.Min(request.Tal1, request.Tal2);
     int maxValue = Math.Max(request.Tal1, request.Tal2);
 
     Random random = new Random();
     int n = random.Next(minValue, maxValue + 1); // +1 for at inkludere maxValue i det tilfældige interval: Random.Next()
-    //string sn = n.ToString();
-
-    // Logging the operation and result to the console
-    //Console.WriteLine($"Tilfældig nummer mellem {x} og {y} er: {n}");
-    //Console.WriteLine($"resultatet sendt: {sn}");
 
     // Sending the result back to the client
     var response = new { Result = n };
-    writer.WriteLine(JsonSerializer.Serialize(new { Result = n }));
-    //writer.WriteLine(sn);
+    writer.WriteLine(JsonSerializer.Serialize(response));
     writer.Flush();
 }
 
 // Method to handle the "add" command
 void HandleAdd(Request request, StreamWriter writer)
 {
-    //writer.WriteLine("Add two numbers: E.g: 1 2");
-    //writer.Flush();
-
-    //string[] messageB = reader.ReadLine().Split(" ");
-    //int a = Int32.Parse(messageB[0]);
-    //int b = Int32.Parse(messageB[1]);
-
-    //int sum = a + b;
-    //string samletSum = sum.ToString();
     int sum = request.Tal1 + request.Tal2;
-
-    // Logging the operation and result to the console
-    //Console.WriteLine($"{a} + {b} = {sum}");
-    //Console.WriteLine($"resultatet sendt: {samletSum}");
 
     // Sending the result back in JSON format to the client
     var response = new { Result = sum };
     writer.WriteLine(JsonSerializer.Serialize(response));
     writer.Flush();
-    //writer.WriteLine(samletSum);
-    //writer.Flush();
 }
 
 // Method to handle the "subtract" command
 void HandleSubtract(Request request, StreamWriter writer)
 {
-    //writer.WriteLine("Subtract two numbers: E.g: 5 2");
-    //writer.Flush();
-
-    //string[] messageC = reader.ReadLine().Split(" ");
-    //int c = Int32.Parse(messageC[0]);
-    //int d = Int32.Parse(messageC[1]);
-
-    //int forskel = c - d;
-    //string samletForskel = forskel.ToString();
     int forskel = request.Tal1 - request.Tal2;
-    string samletForskel = forskel.ToString();
-
-
-    // Logging the operation and result to the console
-    //Console.WriteLine($"{c} - {d} = {forskel}");
-    //Console.WriteLine($"resultatet sendt: {samletForskel}");
 
     // Sending the result back in JSON format to the client
     var response = new { Result = forskel };
     writer.WriteLine(JsonSerializer.Serialize(response));
     writer.Flush();
-    //writer.WriteLine(samletForskel);
-    //writer.Flush();
 }
 
 // Method to send error message
@@ -174,7 +130,6 @@ void SendError(string errorMessage, StreamWriter writer)
     writer.Flush();
 }
 
-
 public class Request
 {
     public string Method { get; set; }
@@ -183,8 +138,8 @@ public class Request
 
     public Request()
     {
-
     }
+
     public Request(string method, int tal1, int tal2)
     {
         Method = method;
@@ -196,6 +151,4 @@ public class Request
     {
         return $"{Method} {Tal1} {Tal2}";
     }
-
-
 }
